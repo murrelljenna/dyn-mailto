2dd9bfabe3dd0e1ae43e19a0201958a58cebb4b1
Jenna <murrelljenna@gmail.com>
2020-10-17 23:33:47 +0000

    Added basic site info to get_fields.php.

--- admin/get_fields.php
+++ admin/get_fields.php
@@ -1,7 +1,13 @@
 <?php
 
 $wp_user = wp_get_current_user()->data;
+$blog_info = get_bloginfo('description');
 
+		ob_start();
+		var_dump($blog_info);
+		$contents = ob_get_contents();
+		ob_end_clean();
+		error_log($contents);
 require_once dirname(__FILE__) . "/field_helpers.php";
 
 return array(
@@ -13,6 +19,10 @@ return array(
 	'user_registered' => $wp_user->user_registered,
 	'user_status' => $wp_user->user_status,
 	'user_ip' => Field_Helpers::get_user_ip(),
+	'site_name' => get_bloginfo('name'),
+	'site_description' => get_bloginfo('description'),
+	'site_url' => get_bloginfo('url'),
+	'admin_email' => get_bloginfo('admin_email'),
 );
 
 ?>
