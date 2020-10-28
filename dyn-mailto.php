b736bea73b10102911654a7d4f9d8bc51ea4a594
Jenna <murrelljenna@gmail.com>
2020-10-28 19:33:23 +0000

    Added blank documentation page for plugin.
    To do:
    
        - Render readme.md as markdown in documentation page.

--- dyn-mailto.php
+++ dyn-mailto.php
@@ -21,9 +21,39 @@ require __DIR__ . '/dyn-mailto-widget.php';
 
 !defined('PLUGIN_DIR') && define('PLUGIN_DIR', dirname(__FILE__));
 
+/* Register widget */
+
 function register_dyn_mailto_widget() 
 {
     register_widget('jmurrell\DynMailto\Widget');
 }
 
 add_action('widgets_init', 'jmurrell\DynMailto\register_dyn_mailto_widget');
+
+/* Register documentation page */
+
+function render_docs() {
+    echo '<div class="wrap">';
+
+    // To do: render markdown documentation from here.
+
+    echo '</div>';
+}
+
+add_action( 'admin_menu', function() {
+    add_submenu_page(
+        null,
+        __( 'Documentation', 'textdomain' ),
+        __( 'Documentation', 'textdomain' ),
+        'manage_options',
+        'dyn-mailto-documentation',
+        'jmurrell\DynMailto\render_docs'
+    );
+} );
+
+add_filter('plugin_action_links_'.plugin_basename(__FILE__), function ( $links ) {
+    $links[] = '<a href="' .
+        admin_url( '/?page=dyn-mailto-documentation' ) .
+        '">' . __('Documentation') . '</a>';
+    return $links;
+});
