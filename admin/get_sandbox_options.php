c03983a06bc3c624e5a7aa22169e7ace31ba3d1c
Jenna <murrelljenna@gmail.com>
2020-10-30 22:15:39 +0000

    Added more twig tags and functions to sandbox options.

--- admin/get_sandbox_options.php
+++ admin/get_sandbox_options.php
@@ -1,10 +1,10 @@
 <?php
 
-$tags = ['if', 'for'];
-$filters = [];
+$tags = ['if', 'for', 'apply'];
+$filters = ['abs', 'capitalize', 'date', 'upper', 'title', 'split', 'round', 'lower'];
 $methods = [];
 $properties = [];
-$functions = ['random'];
+$functions = ['random', 'range', 'date', 'min', 'max'];
 
 return new \Twig\Sandbox\SecurityPolicy($tags, $filters, $methods, $properties, $functions);
 
